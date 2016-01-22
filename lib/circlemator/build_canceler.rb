f03aa6823dd1629041b44557dd1219316fed327c
Emanuel Evans <mail@emanuel.industries>
2016-01-21 18:52:18 -0800

    Completely untested first take
    We don't need no stinkin' tests!

---
+++ lib/circlemator/build_canceler.rb
@@ -0,0 +1,54 @@
+require 'httparty'
+require 'json'
+
+module Circlemator
+  class BuildCanceler
+    def initialize(user: , repo: , current_build: , circle_api_token: )
+      @user = user
+      @repo = repo
+      @current_build = current_build
+      @circle_api_token = circle_api_token
+    end
+
+    def cancel_old_builds!
+      resp = HTTParty.get "https://circleci.com/api/v1/project/#{@user}/#{@repo}",
+                          circle_auth
+      check_response resp
+
+      builds = JSON.parse(resp.body)
+               .select   { |b| %w(running scheduled queued not_running).include? b.fetch('status') }
+               .group_by { |b| b.fetch('branch') }
+               .flat_map { |_, group| group.sort_by { |b| b.fetch('build_num') }[0...-1] }
+               .map      { |b| b.fetch('build_num') }
+
+      cancel_self = !!builds.delete(@current_build)
+
+      builds.each do |build_num|
+        resp = HTTParty.post "https://circleci.com/api/v1/project/#{@user}/#{@repo}/#{build_num}/cancel",
+                             circle_auth
+        check_response resp
+      end
+
+      if cancel_self
+        puts "Daisy, Daisy, give me your answer, do..."
+        HTTParty.post "https://circleci.com/api/v1/project/#{@user}/#{@repo}/#{@current_build}/cancel",
+                      circle_auth
+      end
+    end
+
+    private
+
+    def circle_auth
+      {
+        query: { 'circle-token': @circle_api_token },
+        headers: { 'Accept' => 'application/json' },
+      }
+    end
+
+    def check_response(resp)
+      if resp.code != 200
+        raise "CircleCI API call failed: #{JSON.parse(resp.body)}"
+      end
+    end
+  end
+end
---
+++ lib/circlemator/self_merger.rb
@@ -0,0 +1,69 @@
+require 'httparty'
+require 'json'
+
+module Circlemator
+  class SelfMerger
+    def initialize(user: ,
+                   repo: ,
+                   sha: ,
+                   github_auth_token: ,
+                   base_branch: ,
+                   compare_branch: )
+      @user = user
+      @repo = repo
+      @sha = sha
+      @auth_token = auth_token
+      @base_branch = base_branch
+      @compare_branch = compare_branch
+    end
+
+    def merge!
+      pr_number, pr_url = find_pr
+      return if pr_number.nil? || pr_url.nil?
+
+      msg = "Auto-merge by Circlemator!"
+      response = HTTParty.put "#{pr_url}/merge",
+                              body: { commit_message: msg, sha: @sha }.to_json,
+                              basic_auth: github_auth
+      if response.code != 200
+        body = JSON.parse(response.body)
+        raise "Merge failed: #{body.fetch('message')}"
+      end
+    end
+
+    private
+
+    def find_pr
+      response = HTTParty.get "https://api.github.com/repos/#{github_repo}/pulls",
+                              query: { base: @base_branch },
+                              basic_auth: github_auth
+      if response.code != 200
+        raise "Bad response from Github: #{response.inspect}"
+      end
+
+      prs = JSON.parse(response.body)
+      pr = prs.find do |pr|
+        pr.fetch('head').fetch('ref') == @compare_branch &&
+          pr.fetch('head').fetch('sha') == @sha &&
+          pr.fetch('base').fetch('ref') == @base_branch
+      end
+
+      if pr.nil?
+        puts 'No release PR. Not merging.'
+        return
+      end
+
+      [pr.fetch('number'), pr.fetch('url')]
+    end
+
+
+    def github_repo
+      "#{@user}/#{@repo}"
+    end
+
+
+    def github_auth
+      { username: @auth_token, password: 'x-oauth-basic' }
+    end
+  end
+end
